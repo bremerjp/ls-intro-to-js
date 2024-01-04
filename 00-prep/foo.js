1f13f52b3a9b1f7a73190030740d8d0751cb1b6b
Jacob Bremer <bremerjp@gmail.com>
2024-01-04 10:59:34 -0500

    finished prep section

---
+++ 00-prep/foo.js
@@ -0,0 +1,21 @@
+let foo = "bar";
+console.log(foo);
+foo;
+
+//4. the string 'bar' is the output when the foo.js file is run in node
+// The node repl has an output for each line entered and outputs 'bar' on the last line
+// The chrome console outputs the string 'bar' but is displayed slightly different from node
+
+/*
+5. String - Instance
+Object - Static
+String - Static
+Array, String - Instance
+String Object, Array Nnumber - Instance
+
+
+6. index, lazyDog, operand2
+7. index, CatName, lazyDog, operand2
+8. BIG_NUMBER
+9. snake_case, quick_fox, 1stCharacter
+*/
